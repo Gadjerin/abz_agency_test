1aec74926b3788f6f6be6c558bfb423a2fd28028
Dmytro Kuleshov <gadjerin@gmail.com>
2024-08-28 20:49:49 +0300

    Disable dynamicColor feature

--- app/src/main/java/com/abz/agency/testtask/ui/theme/Theme.kt
+++ app/src/main/java/com/abz/agency/testtask/ui/theme/Theme.kt
@@ -43,7 +43,7 @@ private val LightColorScheme = lightColorScheme(
 fun TesttaskTheme(
     darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
