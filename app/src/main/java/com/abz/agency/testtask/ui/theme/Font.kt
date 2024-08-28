6113aeb1650a373b478057c6f4a67ae459f6747a
Dmytro Kuleshov <gadjerin@gmail.com>
2024-08-28 15:39:29 +0300

    Add Nunito Sans FontFamily and set up app Typography

---
+++ app/src/main/java/com/abz/agency/testtask/ui/theme/Font.kt
@@ -0,0 +1,12 @@
+package com.abz.agency.testtask.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.abz.agency.testtask.R
+
+val NunitoSansFontFamily = FontFamily(
+    Font(resId = R.font.nunito_regular, weight = FontWeight.Normal, style = FontStyle.Normal),
+    Font(resId = R.font.nunito_semibold, weight = FontWeight.SemiBold, style = FontStyle.Normal)
+)
--- app/src/main/java/com/abz/agency/testtask/ui/theme/Type.kt
+++ app/src/main/java/com/abz/agency/testtask/ui/theme/Type.kt
@@ -2,33 +2,37 @@ package com.abz.agency.testtask.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    // In Material3 <h1>/Heading 1 maps to 'displayLarge'.
+    displayLarge = TextStyle(
+        fontFamily = NunitoSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        lineHeight = 24.sp,
+    ),
+    // <p1>/Body 1 maps to 'bodyLarge'.
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NunitoSansFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    // <p2>/Body 2 maps to 'bodyMedium'.
+    bodyMedium = TextStyle(
+        fontFamily = NunitoSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+    ),
+    // <p3>/Body 3 maps to 'bodySmall'.
+    bodySmall = TextStyle(
+        fontFamily = NunitoSansFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+    ),
 )
