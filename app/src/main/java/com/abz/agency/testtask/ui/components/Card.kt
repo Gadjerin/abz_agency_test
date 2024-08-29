e2a98cec056c9dcd9f96f4f4532acaf292110688
Dmytro Kuleshov <gadjerin@gmail.com>
2024-08-29 19:01:44 +0300

    Add DeveloperCard component

---
+++ app/src/main/java/com/abz/agency/testtask/ui/components/Card.kt
@@ -0,0 +1,121 @@
+package com.abz.agency.testtask.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.abz.agency.testtask.R
+import com.abz.agency.testtask.ui.theme.TesttaskTheme
+import com.abz.agency.testtask.ui.theme.TransparentBlackEnabled
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun DeveloperCard(
+    name: String,
+    position: String,
+    email: String,
+    phoneNumber: String,
+    imageUrl: String? = null
+) {
+    ListItem(
+        modifier = Modifier
+            .height(IntrinsicSize.Min),
+        leadingContent = {
+            Box(
+                contentAlignment = Alignment.TopStart,
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                GlideImage(
+                    model = imageUrl,
+                    contentDescription = "Developer photo",
+                    loading = placeholder(R.drawable.photo_placeholder),
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                )
+            }
+        },
+        headlineContent = {
+            Text(
+                name,
+                style = MaterialTheme.typography.bodyMedium,
+                color = TransparentBlackEnabled
+            )
+        },
+        supportingContent = {
+            Column {
+                // Vertical padding is 2*2
+                Spacer(modifier = Modifier.padding(vertical = 2.dp))
+                Text(
+                    position,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Black.copy(alpha = 0.6F)
+                )
+                // Vertical padding is 4*2
+                Spacer(modifier = Modifier.padding(vertical = 4.dp))
+                Text(
+                    email,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = TransparentBlackEnabled,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                // Vertical padding is 2*2
+                Spacer(modifier = Modifier.padding(vertical = 2.dp))
+                Text(
+                    phoneNumber,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = TransparentBlackEnabled
+                )
+                // Space between the last Text and Divider is 24.dp according to the design
+                Spacer(modifier = Modifier.padding(vertical = 12.dp))
+                Divider()
+            }
+        },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DeveloperCardPreview() {
+    TesttaskTheme {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            DeveloperCard(
+                "Malcolm Bailey",
+                "Frontend developer",
+                "jany_murazik51@hotmail.com",
+                "+38 (098) 278 76 24"
+            )
+            DeveloperCard(
+                "Seraphina Anastasia Isolde Aurelia Celestina von Hohenzollern",
+                "Backend developer",
+                "maximus_wilderman_ronaldo_schuppe@gmail.com",
+                "+38 (098) 278 76 24"
+            )
+        }
+    }
+}
